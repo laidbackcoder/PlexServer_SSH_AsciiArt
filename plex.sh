d143fc68a3f7b25ee7f92a0d834fac8e8eb6237a
Phil Tyler <laidbackcoder@gmail.com>
2020-12-29 18:51:07 +0000

    Upload of Local Files

---
+++ plex.sh
@@ -0,0 +1,13 @@
+clear
+
+# http://patorjk.com/software/taag/#p=display&c=echo&f=ANSI%20Regular&t=PLEX
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+echo ""
+echo " ██████  ██      ███████ \e[93m██\e[39m   ██ "
+echo " ██   ██ ██      ██       \e[93m██\e[39m ██  "
+echo " ██████  ██      █████     \e[93m███\e[39m   "
+echo " ██      ██      ██       \e[93m██\e[39m ██  "
+echo " ██      ███████ ███████ \e[93m██\e[39m   ██ "
+echo "                    \e[1mMedia Server"
+echo ""
