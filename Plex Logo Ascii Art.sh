42de2a72977d9bc1797cb5a6a20b7bff29257826
Phil Tyler <laidbackcoder@gmail.com>
2020-12-29 19:46:22 +0000

    Add files via upload

---
+++ Plex Logo Ascii Art.sh
@@ -0,0 +1,10 @@
+clear
+
+echo ""
+echo " ██████  ██      ███████ \e[93m██\e[39m   ██ "
+echo " ██   ██ ██      ██       \e[93m██\e[39m ██  "
+echo " ██████  ██      █████     \e[93m███\e[39m   "
+echo " ██      ██      ██       \e[93m██\e[39m ██  "
+echo " ██      ███████ ███████ \e[93m██\e[39m   ██ "
+echo "                    \e[1mMedia Server"
+echo ""
